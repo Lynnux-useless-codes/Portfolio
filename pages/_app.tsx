53f9d6dcc989eaf401a00e4c99c5162ad2e49442
Dark-LYNN <botdevlynn@gmailcom>
2024-10-12 15:05:35 +0200

    Embed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,11 +17,13 @@ function App({ Component, pageProps }: AppProps) {
     const noHelloDescription = "In digital communication, it's more efficient to ask your question right away instead of using polite but time-consuming greetings or inquiries. This allows for faster responses and asynchronous communication, making everyone happier and more productive."
     const imageOfCodeDescription = "An image of your code is not helpful. When asking a question about a problem with code, people who are volunteering to help need the text of the code. Images of the code are not an acceptable substitute.";
     const dontAskDescription = "Asking questions without stating the problem is bad form and can discourage knowledgeable individuals from responding. The solution is to directly ask the specific question or problem with relevant details, rather than asking if someone is available. Clear and well-formulated questions are essential for effective communication in online communities";
+    const blogNotDevDescription = "Though I've dabbled in several coding languages—Java, JavaScript, Python, Bash, and more—I'm not aiming to be a professional programmer. Coding for me is like digital crafting, something I do for fun, to personalize my online spaces, or solve small problems. I create projects like mini-apps or playful tweaks to websites, not for a portfolio but for my own enjoyment. While I love learning and exploring new skills, I'm not looking for a career in IT. I prefer to keep coding as a casual, creative outlet rather than a full-time pursuit.";
 
     const router = useRouter();
     const isNoHelloPage = router.pathname === '/helper/nohello';
     const isImageOfCodePage = router.pathname === '/helper/imageofcode';
     const isDontAskToAsk = router.pathname === '/helper/dontasktoask';
+    const isBlogNotDev = router.pathname === "/blog/Why-I'm-Not-a-Programmer";
 
     return (
         <>
@@ -72,6 +74,23 @@ function App({ Component, pageProps }: AppProps) {
                         <meta property="og:type" content="website" />
                         <meta property="og:description" content={dontAskDescription}/>
                         <meta name="theme-color" content="#ffffff" />
+                    </>
+                ) : isBlogNotDev ? (
+                    <>
+                        <title>Blog | Why I'm Not A Programmer</title>
+                        <meta property="og:title" content="Why I'm Not A Programmer" />
+                        <link rel="canonical" href={canonical} />
+                        <meta property="og:site_name" content={canonical} />
+                        <meta property="og:description" content={blogNotDevDescription} />
+                        <meta name="description" content={blogNotDevDescription} />
+
+                        {/* Discord Embed */}
+                        <meta property="og:site_name" content="https://lynnux.xyz/" />
+                        <meta property="og:title" content="Why I'm Not A Programmer" />
+                        <meta property="og:url" content={canonical} />
+                        <meta property="og:type" content="website" />
+                        <meta property="og:description" content={blogNotDevDescription}/>
+                        <meta name="theme-color" content="#ff47ff" />
                     </>
                 ) : (
                     <>
