9ae2cc9c955f01007719b0fb55abb841fd6003e6
Dark-LYNN <botdevlynn@gmailcom>
2024-11-02 14:36:15 +0100

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,12 +21,14 @@ function App({ Component, pageProps }: AppProps) {
     const timetravlerTimerDescription = "This timer was started to help timetraveler how far they traveled (only works after June 20 2024 at 6.23pm EST).";
     const overwatchTimerDescription = "A Timer for the next overwatch season, with a default green background so you can chromakey it in obs. \nP.S. add '?overlay=hide' at the end of the url to remove the overlay.";
     const twitchconTimerDescription = "A Timer for the next twitchcon, with a default green background so you can chromakey it in obs. \nP.S. add '?overlay=hide' at the end of the url to remove the overlay."
+    const BlogPostUnspokenRules = "If you’ve ever wondered, “How do I best interact with this person?” you’re not alone! I thought I’d save us all some time by laying out the unspoken rules for interacting with me online. Think of this as a casual guide to avoid any awkward moments and to keep things fun and easygoing between us."
 
     const router = useRouter();
     const isNoHelloPage = router.pathname === '/helper/nohello';
     const isImageOfCodePage = router.pathname === '/helper/imageofcode';
     const isDontAskToAsk = router.pathname === '/helper/dontasktoask';
     const isBlogNotDev = router.asPath === "/blog/Why-I'm-Not-a-Programmer";
+    const isBlogUnspokenRules = router.asPath === "/blog/unspoken_rules";
     const isTimetravlerTimer = router.pathname === '/helper/timers/timetraveler';
     const isOverwatchTimer = router.pathname === '/helper/timers/overwatch';
     const isTwitchconTimer = router.pathname === '/helper/timers/twitchcon';
@@ -147,6 +149,23 @@ function App({ Component, pageProps }: AppProps) {
                         <meta property="og:url" content={canonical} />
                         <meta property="og:type" content="website" />
                         <meta property="og:description" content={twitchconTimerDescription}/>
+                        <meta name="theme-color" content="#ff47ff" />
+                    </>
+                ) : isBlogUnspokenRules? (
+                    <>
+                        <title>Unspoken Rules</title>
+                        <meta property="og:title" content="Unspoken Rules" />
+                        <link rel="canonical" href={canonical} />
+                        <meta property="og:site_name" content={canonical} />
+                        <meta property="og:description" content={BlogPostUnspokenRules} />
+                        <meta name="description" content={BlogPostUnspokenRules} />
+
+                        {/* Discord Embed */}
+                        <meta property="og:site_name" content="https://lynnux.xyz/" />
+                        <meta property="og:title" content="Unspoken Rules" />
+                        <meta property="og:url" content={canonical} />
+                        <meta property="og:type" content="website" />
+                        <meta property="og:description" content={BlogPostUnspokenRules}/>
                         <meta name="theme-color" content="#ff47ff" />
                     </>
                 ) : (
