38081c4e83133673ef7004a33c01fc8940e51268
Dark-LYNN <botdevlynn@gmailcom>
2024-12-07 11:17:43 +0100

    Update

--- public/iframes/script.js
+++ public/iframes/script.js
@@ -1,4 +1,4 @@
-const targetTimestamp = 1729015200 * 1000;
+const targetTimestamp = 1733853600 * 1000;
 
 function getUrlParameter(name) {
     const urlParams = new URLSearchParams(window.location.search);
